593e9926ce6db0038738c75093755f0598db32b3
cesc1 <francesc.aguirre@gmail.com>
2025-02-02 05:46:06 +0100

    [fix] add an import that I missed

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -10,8 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
 #include <stdlib.h>
+#include "libft.h"
 
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
